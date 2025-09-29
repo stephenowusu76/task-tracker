75d712005671cb9d0fbdf86d457cb026567f8449
stephenowusu76 <stephenowusu76@gmail.com>
2025-09-29 22:38:10 +0100

    initial commit

---
+++ TaskTracker.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class TaskTracker {
+    private static int count = 1;
+    public static void main(String[] args) {
+        System.out.println("Welcome to Task Tracker App");
+        System.out.println("Please type help to get the user guide");
+        Scanner in = new Scanner(System.in);
+
+    }
+
+    private static void help() {
+
+    }
+
+    private static void add() {
+
+    }
+
+    private static void update() {
+
+    }
+
+    private static void delete() {
+
+    }
+}
